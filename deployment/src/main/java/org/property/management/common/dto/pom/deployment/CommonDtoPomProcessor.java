e1e629de4c963fd4293cbfc1adea721898dc98fc
matos-nelson <nmatos6@gatech.edu>
2023-04-29 18:08:40 -0500

    Initial baseline commit.

---
+++ deployment/src/main/java/org/property/management/common/dto/pom/deployment/CommonDtoPomProcessor.java
@@ -0,0 +1,14 @@
+package org.property.management.common.dto.pom.deployment;
+
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+
+class CommonDtoPomProcessor {
+
+    private static final String FEATURE = "common-dto-pom";
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem(FEATURE);
+    }
+}
